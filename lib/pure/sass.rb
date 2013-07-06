07f31ef84b70b76e4a576461723c22351874066f
johnotander <johnotander@gmail.com>
2013-07-05 18:17:36 -0600

    Break out engine into own class and require early.

--- lib/pure/sass.rb
+++ lib/pure/sass.rb
@@ -1,12 +1,9 @@
 require "pure/sass/version"
+require "pure/sass/engine"
 require "pure/sass/default_translation"
 require "pure/sass/translation"
 
 module Pure
   module Sass
-    if defined? Rails::Engine
-      class Engine < Rails::Engine
-      end
-    end
   end
 end
