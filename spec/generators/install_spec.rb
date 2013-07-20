530891eb7029f2ad4eab169dbe198d912d10c662
johnotander <johnotander@gmail.com>
2013-07-19 19:39:39 -0600

    Add tests for subdirectories.

--- spec/generators/install_spec.rb
+++ spec/generators/install_spec.rb
@@ -4,6 +4,8 @@ require 'pure/sass/generator'
 describe Pure::Sass::Generator, fakefs: true do
 
   let(:install_path) { File.join(File.dirname(File.dirname(File.dirname(__FILE__))), 'pure') }
+  let(:base_scss_files) { ['all', 'base', 'buttons', 'forms', 'grids', 'menus', 'tables'] }
+
 
   describe 'installation' do
 
@@ -12,8 +14,17 @@ describe Pure::Sass::Generator, fakefs: true do
       Pure::Sass::Generator.new.install
     end
 
-    it 'should add the correct files' do
-      File.exist?(File.join(install_path, 'all.css.scss')).should be_true
+    it 'should add the base files' do
+      base_scss_files.each { |f| File.exist?(File.join(install_path, "#{ f }.css.scss")).should be_true }
+    end
+
+    context 'for subdirectories' do
+
+      let(:subdirs_with_files) { { base: :normalize, buttons: :buttons, forms: :forms } }
+
+      it 'should add the files' do
+        subdirs_with_files.each_pair { |k, f| File.exist?(File.join(install_path, k.to_s, "#{ f }.css.scss")).should be_true }
+      end
     end
   end
 end
