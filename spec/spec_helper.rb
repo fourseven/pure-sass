1dc244ab893dabb6abf9738be084eb9ad1f8a2d1
John Otander <johnotander@gmail.com>
2014-08-22 21:56:29 -0600

    Fix outdated RSpec call.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
 require 'bundler'
 Bundler.require
 
-RSpec.configure do |config|
-  config.color_enabled = true
-end
-
 # From
 # https://github.com/erikhuda/thor/blob/87dc98c6e2c183c9b1f867c6c8e1b62ede5242bc/spec/helper.rb
 def capture(stream)
